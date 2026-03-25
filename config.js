8143e4a982162098530fda31f9090701915c302c
9phoomphi <9phoomphi@gmail.com>
2026-03-25 09:16:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   global.DOC_CONTROL_CONFIG = {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbxutPuoz72aj_fYvBz9f7ldORhWI99atubRwJUZPBgR4HsaQb260pR3JDQBqZsJ6DXE/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbzUS-x_TSblfqDiNNb9Z0sYzFfE3CBdZ57P9jXMDaMWfABBi76fQ6wV35GLb7QtsdGV/exec',
     lockSettings: true
   };
 })(typeof window !== 'undefined' ? window : this);
