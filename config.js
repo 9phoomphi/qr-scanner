953f359acd946dd94b7083e9ce68e7031b9f3d52
9phoomphi <9phoomphi@gmail.com>
2026-04-02 06:58:34 +0700

    +

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   global.DOC_CONTROL_CONFIG = {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbx7rTP7ZI9b-rG4wDNdfhXNVxjoUJqRkF5J7Fwu0zhPsuJ8C23stP09gqht6-OJlguc/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbw_dHmo0i8wfsnurwxmpNdrjXh4hwUIJ3FRQ0u06oj2qk9MclkqFtjzkiac7TvYkrVE/exec',
     lockSettings: true
   };
 })(typeof window !== 'undefined' ? window : this);
